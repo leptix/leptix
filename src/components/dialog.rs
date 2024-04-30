21baf376aa4f9800b583a90c4f673dff9527a46d
Upbolt <nnrv.ous@gmail.com>
2024-04-30 14:21:05 -0700

    add dialog anatomy

---
+++ src/components/dialog.rs
@@ -0,0 +1,61 @@
+use leptos::*;
+
+#[component]
+pub fn DialogRoot() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogTrigger() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogPortal() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogOverlay() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogContent() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogContentModal() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogContentNonModal() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogContentImpl() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogTitle() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+pub fn DialogClose() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+fn TitleWarning() -> impl IntoView {
+  view! {}
+}
+
+#[component]
+fn DescriptionWarning() -> impl IntoView {
+  view! {}
+}
