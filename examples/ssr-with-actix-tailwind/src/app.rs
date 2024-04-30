72cf34ce8cadc153dfc519a39a7850ed26155eb3
Upbolt <nnrv.ous@gmail.com>
2024-04-30 00:42:44 -0700

    added ssr example

---
+++ examples/ssr-with-actix-tailwind/src/app.rs
@@ -0,0 +1,175 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+use leptos_use::{use_cookie, utils::FromToStringCodec};
+
+use crate::primitives::PrimitivesShowcase;
+
+#[component]
+pub fn App() -> impl IntoView {
+  provide_meta_context();
+
+  view! {
+      <Stylesheet id="leptos" href="/pkg/ssr-with-actix-tailwind.css"/>
+      <Title text="Leptix SSR"/>
+
+      <Router>
+          <main class="dark:bg-[#111113] p-4 flex flex-col gap-2 text-mauve11 dark:text-white">
+              <Routes>
+                  <Route path="" view=HomePage/>
+                  <Route path="/*any" view=NotFound/>
+              </Routes>
+          </main>
+      </Router>
+  }
+}
+
+#[component]
+fn HomePage() -> impl IntoView {
+  view! {
+      <Auth />
+      <PrimitivesShowcase />
+  }
+}
+
+#[component]
+fn Auth() -> impl IntoView {
+  let (auth_cookie, _) = use_cookie::<String, FromToStringCodec>("auth");
+  let profile = create_resource(|| (), |_| async move { get_profile().await.ok() });
+  let auth_signal = move || profile.get().or(Some(auth_cookie.get())).flatten();
+
+  view! {
+
+    {move || if auth_signal()
+      .map(|auth_cookie| auth_cookie == "bob")
+      .unwrap_or(false)
+    {
+      view! {
+        <div class="flex gap-2 items-center">
+            <span>"hello bob"</span>
+            <button
+                class="transition duration-75 rounded-md hover:bg-violet8 bg-violet9 active:bg-violet10 px-1.5 py-1 text-white text-sm font-semibold"
+                on:click=move |_| {
+                    spawn_local(async move {
+                        _ = logout().await;
+                        profile.refetch();
+                    });
+                }
+            >
+                "Logout"
+            </button>
+        </div>
+      }
+      .into_view()
+    } else {
+      view! {
+        <button
+          class="transition duration-75 rounded-md hover:bg-violet8 bg-violet9 active:bg-violet10 px-1.5 py-1 text-white text-sm font-semibold w-fit"
+          on:click=move |_| {
+            spawn_local(async move {
+              _ = login().await;
+              profile.refetch();
+            });
+          }
+        >
+          "Login"
+        </button>
+      }
+      .into_view()
+    }}
+  }
+}
+
+#[server]
+async fn get_profile() -> Result<String, ServerFnError> {
+  use actix_web::HttpRequest;
+  use leptos::server_fn::error::NoCustomError;
+  use leptos_actix::extract;
+
+  let foo = extract::<HttpRequest>()
+    .await?
+    .cookie("auth")
+    .ok_or(ServerFnError::<NoCustomError>::ServerError(
+      "unauthorized".to_string(),
+    ))?
+    .value()
+    .to_string();
+
+  Ok(foo)
+}
+
+#[server]
+async fn login() -> Result<(), ServerFnError> {
+  use actix_web::{
+    cookie::{
+      time::{Duration, OffsetDateTime, Time},
+      Cookie,
+    },
+    http::header,
+    http::header::HeaderValue,
+  };
+  use leptos_actix::ResponseOptions;
+  use std::ops::Add;
+
+  let mut cookie = Cookie::build("auth", "bob")
+    .max_age(Duration::days(7))
+    .expires(OffsetDateTime::now_utc() + Duration::days(7))
+    .http_only(true)
+    .path("/")
+    .finish();
+
+  if let Ok(cookie) = HeaderValue::from_str(&cookie.to_string()) {
+    expect_context::<ResponseOptions>().insert_header(header::SET_COOKIE, cookie);
+  }
+
+  Ok(())
+}
+
+#[server]
+async fn logout() -> Result<(), ServerFnError> {
+  use actix_web::{
+    cookie::{
+      time::{Duration, OffsetDateTime, Time},
+      Cookie,
+    },
+    http::header,
+    http::header::HeaderValue,
+  };
+  use leptos_actix::ResponseOptions;
+  use std::ops::Add;
+
+  let mut cookie = Cookie::build("auth", "")
+    .max_age(Duration::seconds(0))
+    .expires(OffsetDateTime::now_utc() - Duration::seconds(1))
+    .http_only(true)
+    .path("/")
+    .finish();
+
+  if let Ok(cookie) = HeaderValue::from_str(&cookie.to_string()) {
+    expect_context::<ResponseOptions>().insert_header(header::SET_COOKIE, cookie);
+  }
+
+  Ok(())
+}
+
+/// 404 - Not Found
+#[component]
+fn NotFound() -> impl IntoView {
+  // set an HTTP status code 404
+  // this is feature gated because it can only be done during
+  // initial server-side rendering
+  // if you navigate to the 404 page subsequently, the status
+  // code will not be set because there is not a new HTTP request
+  // to the server
+  #[cfg(feature = "ssr")]
+  {
+    // this can be done inline because it's synchronous
+    // if it were async, we'd use a server function
+    let resp = expect_context::<leptos_actix::ResponseOptions>();
+    resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+  }
+
+  view! {
+      <h1>"Not Found"</h1>
+  }
+}
--- src/components/scroll_area.rs
+++ src/components/scroll_area.rs
@@ -163,38 +163,14 @@ pub fn ScrollAreaViewport(
   #[prop(optional)] nonce: Option<MaybeSignal<String>>,
 
   #[prop(attrs)] attrs: Attributes,
-  #[prop(optional)] node_ref: NodeRef<AnyElement>,
+  //#[prop(optional)] node_ref: NodeRef<AnyElement>,
   children: Children,
 ) -> impl IntoView {
   let context = use_context::<ScrollAreaContextValue>()
     .expect("ScrollAreaViewport must be used in a ScrollAreaRoot component");
 
   let mut merged_attrs = attrs.clone();
-  merged_attrs.extend(
-    [
-      ("data-primitive-scroll-area-viewport", "".into_attribute()),
-      (
-        "style",
-        Signal::derive(move || {
-          format!(
-            "overflow-x: {}; overflow-y: {}; ",
-            if context.scrollbar_x_enabled.get() {
-              "scroll"
-            } else {
-              "hidden"
-            },
-            if context.scrollbar_y_enabled.get() {
-              "scroll"
-            } else {
-              "hidden"
-            }
-          )
-        })
-        .into_attribute(),
-      ),
-    ]
-    .into_iter(),
-  );
+  merged_attrs.extend([("data-primitive-scroll-area-viewport", "".into_attribute())]);
 
   let content_ref = context.content;
 
@@ -214,12 +190,44 @@ pub fn ScrollAreaViewport(
   //   (context.on_content_change)(content);
   // });
 
+  Effect::new(move |_| {
+    let Some(viewport) = context.viewport.get() else {
+      return;
+    };
+
+    _ = viewport
+      .style(
+        "overflow-x",
+        if context.scrollbar_x_enabled.get() {
+          "scroll"
+        } else {
+          "hidden"
+        },
+      )
+      .style(
+        "overflow-y",
+        if context.scrollbar_y_enabled.get() {
+          "scroll"
+        } else {
+          "hidden"
+        },
+      );
+  });
+
   view! {
     <>
-      <style
-        inner_html="[data-primitive-scroll-area-viewport]{scrollbar-width:none;-ms-overflow-style:none;-webkit-overflow-scrolling:touch;}[data-primitive-scroll-area-viewport]::-webkit-scrollbar{display:none}"
-        nonce=nonce.into_attribute()
-      />
+      <style nonce=nonce.into_attribute()>
+        r"[data-primitive-scroll-area-viewport] {
+            scrollbar-width:none;
+            -ms-overflow-style:none;
+            -webkit-overflow-scrolling:touch;
+        }
+
+        [data-primitive-scroll-area-viewport]::-webkit-scrollbar{
+            display:none
+        }"
+      </style>
+
       <Primitive
         element=html::div
         attrs=merged_attrs
