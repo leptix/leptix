869b98c4e9a84113800846616d10a221271b3f55
Upbolt <nnrv.ous@gmail.com>
2024-06-07 07:12:05 -0700

    swap out LabelRoot #[prop(optional)] Option<Callback<T>> for #[prop(default, into)] Callback<T>

--- crates/primitives/src/components/label.rs
+++ crates/primitives/src/components/label.rs
@@ -5,11 +5,11 @@ use crate::{components::primitive::Primitive, Attributes};
 
 #[component]
 pub fn LabelRoot(
+  #[prop(optional)] for_html: Option<MaybeSignal<String>>,
+  #[prop(default=Callback::new(|_:MouseEvent|{}), into)] on_mouse_down: Callback<MouseEvent>,
+  #[prop(optional)] node_ref: NodeRef<AnyElement>,
   #[prop(attrs)] attrs: Attributes,
   children: Children,
-  #[prop(optional)] for_html: Option<MaybeSignal<String>>,
-  #[prop(optional)] on_mouse_down: Option<Callback<MouseEvent>>,
-  #[prop(optional)] node_ref: NodeRef<AnyElement>,
 ) -> impl IntoView {
   let mut merged_attrs = attrs.clone();
   merged_attrs.push((
@@ -23,9 +23,7 @@ pub fn LabelRoot(
       attrs=merged_attrs
       node_ref=node_ref
       on:mousedown=move |ev: MouseEvent| {
-        if let Some(on_mouse_down) = on_mouse_down {
           on_mouse_down.call(ev.clone());
-        }
 
         if ev.default_prevented() && ev.detail() > 1 {
           ev.prevent_default();
